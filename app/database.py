e00c7ac9ba15b68de0e825e7ced0dbbec4edb8f0
Ashley Fitzgerald <ashleyf1996@gmail.com>
2025-11-05 09:28:44 +0000

    FEAT: Fixed table structure, created table in DB

--- app/database.py
+++ app/database.py
@@ -1,4 +1,15 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData, Table, Boolean, Column, Integer, String
+
 
 engine = create_engine('sqlite:///topicley.db', echo = True) #ECHO = true prints swl commands to terminal
+meta = MetaData()
 
+cases = Table(
+    'cases', 
+    meta,
+    Column('id', Integer, primary_key = True),
+    Column('title', String),
+    Column('solved', Boolean)
+)
+
+meta.create_all(engine)
